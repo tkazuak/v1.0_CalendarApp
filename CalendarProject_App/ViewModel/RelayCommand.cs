48ddcba8d7fdf1ad36b77f8fccabc46f4ec8106e
tkazuak <tkazuak@gmail.com>
2024-11-28 01:41:17 +0900

    Add CalendarProject_App

---
+++ CalendarProject_App/ViewModel/RelayCommand.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CalendarProject_App.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
+
+        /// <summary>
+        /// コマンドが実行可能かどうかが変わったときに発生するイベント
+        /// </summary>
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>
+        /// コンストラクタ: 実行ロジックのみ指定する場合
+        /// </summary>
+        /// <param name="execute">コマンドの実行ロジック</param>
+        public RelayCommand(Action<object?> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// コンストラクタ: 実行ロジックと実行可否ロジックを指定する場合
+        /// </summary>
+        /// <param name="execute">コマンドの実行ロジック</param>
+        /// <param name="canExecute">コマンドの実行可否ロジック</param>
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// コマンドが実行可能かどうかを判断する
+        /// </summary>
+        /// <param name="parameter">コマンドパラメータ</param>
+        /// <returns>実行可能ならtrue、そうでなければfalse</returns>
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        /// <summary>
+        /// コマンドを実行する
+        /// </summary>
+        /// <param name="parameter">コマンドパラメータ</param>
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ CalendarProject_App/ViewModel/ViewModelBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarProject_App.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// プロパティ変更通知を送信する
+        /// </summary>
+        /// <param name="propertyName">変更されたプロパティ名</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName ?? string.Empty));
+        }
+
+        /// <summary>
+        /// フィールド値を更新し、プロパティ変更通知を送信する
+        /// </summary>
+        /// <typeparam name="T">プロパティの型</typeparam>
+        /// <param name="field">変更する対象のフィールド</param>
+        /// <param name="value">新しい値</param>
+        /// <param name="propertyName">プロパティ名</param>
+        /// <returns>値が変更された場合はtrue、変更されなかった場合はfalse</returns>
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// IDisposableの実装: 必要ならリソースを解放する
+        /// </summary>
+        public virtual void Dispose()
+        {
+            // 派生クラスでリソース解放処理が必要な場合にオーバーライドする
+        }
+    }
+}
