6c84a75aab718375e7ed133c749e88310fd4b79a
tkazuak <tkazuak@gmail.com>
2024-11-28 00:46:27 +0900

    初回コミット。アセンブリファイルの追加

--- HeaderProject/obj/Debug/net8.0-windows/HeaderProject.AssemblyInfo.cs
+++ HeaderProject/obj/Debug/net8.0-windows/HeaderProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeaderProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caff6e17c8ce4106eb21ff7c70eed649a0f1188b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeaderProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeaderProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
