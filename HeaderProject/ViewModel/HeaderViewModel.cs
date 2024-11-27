88e40e8dbf93ab11c453098b79bbf1f2f8a26d50
tkazuak <tkazuak@gmail.com>
2024-11-28 00:03:51 +0900

    Add HeaderProject, ContentProject, FooterProject

---
+++ HeaderProject/ViewModel/HeaderViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeaderProject.ViewModel
+{
+    public class HeaderViewModel
+    {
+        public string HeaderText { get; set; }
+
+        public HeaderViewModel()
+        {
+            HeaderText = "Header Content from ViewModel";
+        }
+    }
+}
+
